128b9df37631237ec536b2317e7852c11babc952
aleblancbio <aleblanc.bio@gmail.com>
2022-03-24 12:07:06 -0400

    Updating tests

--- tests/testthat/testTimeScale.R
+++ tests/testthat/testTimeScale.R
@@ -1,7 +1,6 @@
-test_that("Composition of timeScale direct and inverse return initial values", {
+test_that("Composition of timeScale direct and inverse return initial values (non zero rate)", {
   conditions <- data.frame(time = seq(0,30,length.out = 10), temp = 20 + rnorm(10, 10, 5))
   model <- "modelLinear"
-  interpolation = "linear"
   param = list(a = 1, T0 = 10)
   
   #Initial time
@@ -10,13 +9,34 @@ test_that("Composition of timeScale direct and inverse return initial values", {
   
   #Scaled time
   y1 <- rep(0,10,length.out = 10)
-  y2 <- timeScale(x1, x2, model = model, conditions = conditions, param = param, interpolation = interpolation)
+  y2 <- timeScale(x1, x2, model = model, conditions = conditions, param = param)
 
   #Scaling back to original time
-  x2Calc <- timeScale(y1, y2, model = model, conditions = conditions, param = param, interpolation = interpolation, inverse = TRUE)
+  x2Calc <- timeScale(y1, y2, model = model, conditions = conditions, param = param, inverse = TRUE)
   
   #Equality given some rounding
   expect_equal(x2, x2Calc, tolerance=1e-3)
+})
+
+test_that("Composition of timeScale direct and inverse return initial values (with zero rate)", {
+  conditions <- data.frame(time = seq(0,30,length.out = 10), temp = rnorm(10, 10, 5))
+  model <- "modelLinear"
+  param = list(a = 1, T0 = 10)
+  
+  #Initial time
+  x1 = rep(0,10,length.out = 10)
+  x2 = seq(11,20,length.out = 10)
+  
+  #Scaled time
+  y1 <- rep(0,10,length.out = 10)
+  y2 <- timeScale(x1, x2, model = model, conditions = conditions, param = param)
+  
+  #Scaling back to original time
+  x2Calc <- timeScale(y1, y2, model = model, conditions = conditions, param = param, inverse = TRUE)
+  
+  #Equality given some rounding
+  expect_equal(x2, x2Calc, tolerance=1e-3)
+
 })
 
 test_that("timeScale give proper values for simple model (constant interpolation)", {
@@ -31,7 +51,7 @@ test_that("timeScale give proper values for simple model (constant interpolation
   x2 = conditions$time
   
   #Scaled time
-  y2 <- timeScale(x1, x2, model = model, conditions = conditions, param = param, interpolation = interpolation)
+  y2 <- timeScale(x1, x2, model = model, conditions = conditions, param = param)
   answer = c(0,cumsum(modelLinear(conditions$temp[seq(1,n-1)],param = list(a = 1, T0 = 10))))
   
   #Equality given some rounding
@@ -51,27 +71,10 @@ test_that("timeScale example with modelGDD", {
   x2 = conditions$time
   
   #Scaled time
-  y2 <- timeScale(x1, x2, model = model, conditions = conditions, param = param, interpolation = interpolation, control = control)
+  y2 <- timeScale(x1, x2, model = model, conditions = conditions, param = param, control = control)
   answer = c(0,cumsum(modelGDD(conditions$Tmin[seq(1,n-1)],conditions$Tmax[seq(1,n-1)],param = list(a = 1, T0 = 10))))
   
   #Equality given some rounding
   expect_equal(y2, answer, tolerance=1e-3)
 })
 
-test_that("timeScale can integrate on medium oscillating dataset", {
-  
-  time <- seq(0, 10, by = 1/24)
-  temp <- rnorm(length(time), mean = 5, sd = 0.5)*sin(2*pi*time) + 10
-  conditions <- data.frame(time = time, temp = temp)
-  model <- "modelLinear"
-  param <- list(T0 = 10, a = 1)
-  x1 = rep(0,length.out = 10)
-  x2 = seq(1,10,length.out = 10)
-  z2 <- timeScale(x1, x2, model = model, conditions = conditions, param = param, interpolation = "linear")
-  
-  z1 <- rep(0,10,length.out = 10)
-  x2Calc <- timeScale(z1, z2, model = model, conditions = conditions, param = param, interpolation = "linear", inverse = TRUE)
-  x2Calc
-  
-  expect_error(timeScale(x1, x2, model = model, conditions = conditions, param = param, interpolation = "linear"), NA)
-})
--- tests/testthat/testTimeShift.R
+++ tests/testthat/testTimeShift.R
@@ -1,6 +1,5 @@
-test_that("Composition timeScale on timeShift return initial values of scaledPeriod", {
+test_that("Composition timeScale on timeShift return initial values of scaledPeriod (non zero rate)", {
 conditions <- data.frame(time = seq(0,30,length.out = 10), temp = 30 + rnorm(10, 10, 5))
-condModel <- interpolateCond(conditions, method = "linear")
 model <- "modelLinear"
 param = list(a = 1, T0 = 10)
 
@@ -9,10 +8,30 @@ x1 = rep(0,10,length.out = 10)
 scaledPeriod = rep(10,10)
 
 #Compute x2 scaled period
-x2 <- timeShift(x1, scaledPeriod = scaledPeriod, model = model, conditions = conditions, param = param, interpolation = "linear")
-scaledPeriodCalc <- timeScale(x1, x2, model = model, conditions = conditions, param = param, interpolation = "linear")
-
+x2 <- timeShift(x1, scaledPeriod = scaledPeriod, model = model, conditions = conditions, param = param)
+scaledPeriodCalc <- timeScale(x1, x2, model = model, conditions = conditions, param = param)
 
 #Equality given some rounding
 expect_equal(scaledPeriod, scaledPeriodCalc, tolerance=1e-3)
+})
+
+test_that("Composition timeScale on timeShift return initial values of scaledPeriod (zero rate)", {
+  conditions <- data.frame(time = seq(0,30,length.out = 10), temp = rnorm(10, 15, 5))
+  model <- "modelLinear"
+  param = list(a = 1, T0 = 10)
+  
+  #Initial time
+  x1 = rep(0,10,length.out = 10)
+  scaledPeriod = rep(10,10)
+  
+  #Compute x2 scaled period
+  x2 <- timeShift(x1, scaledPeriod = scaledPeriod, model = model, conditions = conditions, param = param)
+  scaledPeriodCalc <- timeScale(x1, x2, model = model, conditions = conditions, param = param)
+  
+  #Equality given some rounding
+  expect_equal(scaledPeriod, scaledPeriodCalc, tolerance=1e-3)
+})
+
+test_that("timeShift return proper values for a simple case (with zero rate)", {
+#to be written (important)
 })
