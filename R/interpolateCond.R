274c970c2fa4879978c050a09804cb8ca42a28e8
aleblancbio <aleblanc.bio@gmail.com>
2022-04-28 13:52:58 -0400

    Uniformize the use of time as a variable for composeModel and interpolateCond. Also minor documentation and tests updates.

--- R/interpolateCond.R
+++ R/interpolateCond.R
@@ -7,14 +7,13 @@
 #' @export
 #' @examples
 #' #Example of a call with two variables
-#' conditions <- data.frame(time = seq(0,30,length.out = 10), temp = rnorm(10, 10, 5), hr = rnorm(10, 10, 5))
-#' f <- interpolateCond(conditions, method = "linear")
+#' conditions <- data.frame(time = seq(0,30,length.out = 10), temp = rnorm(10, 10, 5), rh = rnorm(10, 10, 5))
+#' f <- interpolateCond(conditions, method = "constant")
 #' f
 #' #Plotting the result for temp 
 #' plot(conditions$time,conditions$temp)
 #' x <- seq(0,30,length.out = 100)
 #' lines(x,f$temp(x), lty = 1)
-#' 
 #' f <- interpolateCond(conditions, method = "spline")
 #' #Plotting the result for temp 
 #' plot(conditions$time,conditions$temp)
@@ -22,21 +21,32 @@
 #' lines(x,f$temp(x), lty = 1)
 setGeneric("interpolateCond", function(conditions, method) standardGeneric("interpolateCond"))
 setMethod("interpolateCond", signature(conditions = "data.frame", method = "character"), function(conditions, method) {
-  #Interpolation
-  interpolatedCond <- list()
-  if(method %in% c("constant","linear")){
-    #Constant and linear cases
-    for(i in variableConditions(conditions)){
-      interpolatedCond[[i]] <- approxfun(x = conditions$time, y = conditions[[i]], method = method, rule = 1, f = 0, ties = mean)
-    }
-  }else if(method %in% c("spline")){
-      #Spline case (natural cubic)
-      for(i in variableConditions(conditions)){
-        interpolatedCond[[i]] <- splinefun(x = conditions$time, y = conditions[[i]], method = "natural", ties = mean)
+
+     #Define a function that would generate interpolated functions in function of time only, for a predefined conditions variable and method
+    interpolatedCondVar <- function(variable, conditions, method){
+      force(variable)
+      if(method %in% c("constant","linear")){
+        #Constant and linear cases
+        f <- function(time){
+          approx(xout = time, x = conditions$time, y = conditions[[variable]], method = method, rule = 1, f = 0, ties = mean)$y
+        }
+      }else if(method %in% c("spline")){
+        #Spline case (natural cubic)
+        f <- function(time){
+          spline(xout = time, x = conditions$time, y = conditions[[variable]], method = "natural", ties = mean)$y
+        }
+      }else{
+        stop("Undefined method of interpolation for conditions")
       }
-  }else{
-    stop("Undefined method of interpolation for conditions")
-  }
+
+      return(f)
+    }
+
+    ##Generate and store functions
+    interpolatedCond <- list()
+    for(i in variableConditions(conditions)){
+      interpolatedCond[[i]] <- interpolatedCondVar(i, conditions, method)
+    }
+
   return(interpolatedCond)
-  
 })
